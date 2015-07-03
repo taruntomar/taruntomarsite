113e9a45dc03b42b5b634245309c65ade6e648a9
taruntomar <taruntomar64@gmail.com>
2015-07-03 17:43:02 +0530

    First commit of site code

---
+++ taruntomarmvc/Controllers/ArticlesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace taruntomarmvc.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        //
+        // GET: /Articles/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ taruntomarmvc/Controllers/ProjectsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace taruntomarmvc.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        //
+        // GET: /Projects/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
